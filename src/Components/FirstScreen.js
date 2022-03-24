52b26ec347f7d51482249e7a7d5376215edbf37c
carolxavier <xavier.biotec@gmail.com>
2022-03-24 15:02:02 -0300

    Got movies from API and displaied them on page

--- src/Components/FirstScreen.js
+++ src/Components/FirstScreen.js
@@ -1,16 +1,41 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import Header from './Header';
 
-function FirstScreen(){
-    return(
-        <section>
+function FirstScreen() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const promise = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+
+        promise.then(response => {
+            setMovies(response.data);
+        });
+
+        // promise.catch(response.error.status); 
+    }, []);
+
+
+    return movies ? (
+        <section className='first-screen'>
             <Header />
             <h2>Selecione o filme</h2>
             <div className="movie-collection">
-                <div>Coluna1</div>
-                <div>Coluna2</div>
+                
+                    {
+                        movies.map(movie => {
+                            const { id, posterURL, title } = movie;
+                            return <div className='poster-container' key={id}>
+                                <img className='poster' src={posterURL} alt={title}  />
+                            </div>
+                        })
+                    }
+                
             </div>
         </section>
+    ) : (
+        <p>Carregando...</p>
     )
 }
 
